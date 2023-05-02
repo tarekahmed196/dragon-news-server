77d6bf57b5c5112abaf50e6d07aa438b7efbd0eb
tarekahmed196 <tarekjustcse@gmail.com>
2023-05-02 20:36:01 +0600

    start in scripts added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express')
 const app = express();
 const cors = require('cors');
@@ -6,6 +7,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
+const news = require('./data/news.json')
 
 app.get('/', (req, res)=>{
     res.send('News API running');
@@ -13,6 +15,29 @@ app.get('/', (req, res)=>{
 
 app.get('/news-categories', (req, res)=>{
     res.send(categories)
+});
+
+app.get('/category/:id', (req, res)=>{
+    const id = req.params.id;
+    if(id==='08'){
+        res.send(news);
+    }
+    else{
+        const categoryNews = news.filter(n=>n.category_id === id)
+    res.send(categoryNews);
+    }
+    
+})
+
+app.get('/news', (req, res)=>{
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res)=>{
+    const id = req.params.id;
+    const selected_news = news.find(n =>n._id ===id);
+    res.send (selected_news);
+    
 })
 
 app.listen(port, ()=>{
